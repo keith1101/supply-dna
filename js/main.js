8688363d829e2e5efba4925f5845457c5a39fd22
Huy IT <kaigaming5565@gmail.com>
2025-06-26 08:31:37 +0700

    feat: add files

---
+++ js/main.js
@@ -0,0 +1,5 @@
+// This file is currently empty, but ready for your custom JavaScript code.
+// For example, you might add logic for form submissions, animations, or dynamic content loading here.
+
+// Example: Simple console log to confirm script is loaded
+console.log("Supply DNA main.js loaded!");
